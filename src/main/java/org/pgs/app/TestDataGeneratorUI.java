6328618a9de3e55198af346d005ce463db5a1bfc
paramesh-eswar <88368512+paramesh-eswar@users.noreply.github.com>
2023-01-13 00:11:09 +0530

    UI changes for status message

--- src/main/java/org/pgs/app/TestDataGeneratorUI.java
+++ src/main/java/org/pgs/app/TestDataGeneratorUI.java
@@ -10,11 +10,17 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.Document;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 
 public class TestDataGeneratorUI extends JFrame {
 
@@ -22,6 +28,7 @@ public class TestDataGeneratorUI extends JFrame {
 	private JPanel contentPane;
 	private JTextField numOfRowsTxtFld;
 	private JTextField filenameTxtFld;
+	private TestDataGeneratorV3 tdg = new TestDataGeneratorV3();
 
 	/**
 	 * Launch the application.
@@ -83,6 +90,35 @@ public class TestDataGeneratorUI extends JFrame {
 		
 		JButton uploadFileBtn = new JButton("Upload File");
 		uploadFileBtn.setBounds(574, 126, 133, 34);
+		contentPane.add(uploadFileBtn);
+		
+		JButton btnGenerateData = new JButton("Generate Data");
+		btnGenerateData.setBounds(317, 169, 181, 44);
+		btnGenerateData.setEnabled(false);
+		contentPane.add(btnGenerateData);
+		
+//		JTextPane resultPane = new JTextPane();
+//		resultPane.setEnabled(false);
+//		resultPane.setEditable(false);
+//		resultPane.setBounds(50, 225, 679, 186);
+//		contentPane.add(resultPane);
+		
+		JLabel tdgMessage = new JLabel("");
+		tdgMessage.setBounds(75, 200, 133, 23);
+		tdgMessage.setFont(new Font("serif", Font.PLAIN, 14));
+		contentPane.add(tdgMessage);
+		
+		JTextArea resultPane = new JTextArea();
+		resultPane.setEnabled(false);
+		resultPane.setEditable(false);
+		resultPane.setWrapStyleWord(true);
+		resultPane.setLineWrap(true);
+		resultPane.setFont(new Font("serif", Font.PLAIN, 14));
+		
+		JScrollPane scrollPane = new JScrollPane(resultPane, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setBounds(50, 225, 679, 186);
+		contentPane.add(scrollPane);
+		
 		uploadFileBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				if(event.getSource() == uploadFileBtn) {
@@ -91,36 +127,56 @@ public class TestDataGeneratorUI extends JFrame {
 					if(response == JFileChooser.APPROVE_OPTION) {
 						String filePath = metadataFileUpload.getSelectedFile().getAbsolutePath();
 						filenameTxtFld.setText(filePath);
+						resultPane.setText(null);
+						btnGenerateData.setEnabled(true);
+						tdg.errorMessage.setLength(0);
 					}
 				}
 			}
 		});
-		contentPane.add(uploadFileBtn);
 		
-		JTextPane resultPane = new JTextPane();
-		resultPane.setEnabled(false);
-		resultPane.setEditable(false);
-		resultPane.setBounds(50, 225, 679, 186);
-		contentPane.add(resultPane);
-		
-		JButton btnGenerateData = new JButton("Generate Data");
-		btnGenerateData.setBounds(317, 169, 181, 44);
 		btnGenerateData.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				if(event.getSource() == btnGenerateData) {
+					btnGenerateData.setEnabled(false);
 					String filePath = filenameTxtFld.getText();
-					Long numOfRows = Long.valueOf(numOfRowsTxtFld.getText());
-					long startTime = System.currentTimeMillis();
-					TestDataGeneratorV3 tdg = new TestDataGeneratorV3();
-					boolean isDataGenerated = tdg.generateTestData(filePath, numOfRows);
-					long endTime = System.currentTimeMillis();
-					if(isDataGenerated)
-						resultPane.setText("Time taken to generate test data: " + ((endTime-startTime)/1000) + " sec");
-					else
-						resultPane.setText("Test data genearation failed with errors!!");
+					Long numOfRows = 0l;
+					try {
+						numOfRows = Long.valueOf(numOfRowsTxtFld.getText());
+						if(numOfRows<=0) {
+							TestDataGeneratorUI.this.dispose();
+							JOptionPane.showMessageDialog(null,"Number of rows should be whole number", "Invalid number of rows!", JOptionPane.INFORMATION_MESSAGE);
+						} else {
+//							Document doc = resultPane.getDocument();
+//							SimpleAttributeSet keyWord = new SimpleAttributeSet();
+//							StyleConstants.setBold(keyWord, true);
+							try {
+//								resultPane.getDocument().remove(0, doc.getLength());
+//								resultPane.getDocument().insertString(0, "", null);
+								resultPane.setText("Test data generation is in progress ...\n");
+								tdgMessage.setText("Test data generation is in progress ...\n");
+//								doc.insertString(0, "Test data generation is in progress ...\n", keyWord);
+								long startTime = System.currentTimeMillis();
+								boolean isDataGenerated = tdg.generateTestData(filePath, numOfRows);
+//								TestDataGeneratorUI.this.dispose();
+								long endTime = System.currentTimeMillis();
+								resultPane.append(tdg.errorMessage.toString() + "\n");
+//							    doc.insertString(doc.getLength(), tdg.errorMessage.toString() + "\n", keyWord);
+							    if(isDataGenerated) {
+							    	JOptionPane.showMessageDialog(null, tdg.errorMessage.toString() + "\nTime taken to generate test data: " + ((endTime-startTime)/1000) + " sec", "Data generated successfully!", JOptionPane.INFORMATION_MESSAGE);
+							    	resultPane.append("Time taken to generate test data: " + ((endTime-startTime)/1000) + " sec");
+//							    	doc.insertString(doc.getLength(), "Time taken to generate test data: " + ((endTime-startTime)/1000) + " sec", keyWord);
+							    }
+							} catch (Exception e) {
+								System.out.println(e);
+							}
+						}
+					} catch (NumberFormatException nfe) {
+						JOptionPane.showMessageDialog(null,"Number of rows should be whole number", "Invalid number of rows!", JOptionPane.INFORMATION_MESSAGE);
+					}
 				}
+//				btnGenerateData.setEnabled(true);
 			}
 		});
-		contentPane.add(btnGenerateData);
 	}
 }
